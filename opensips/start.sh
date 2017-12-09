c004d855129dc3952c7688e0141734062d0a36e2
root <root@localhost.localdomain>
2017-12-09 22:31:40 +0200

    after editing run.sh

--- opensips/start.sh
+++ opensips/start.sh
@@ -8,8 +8,9 @@ opensipsdbctl create
 opensipsctl add 700@127.0.0.1 700p
 opensipsctl add 800@127.0.0.1 800p
 service rsyslog start
-/usr/local/sbin/opensipsctl start
 HOST_IP=$(ip route get 8.8.8.8 | head -n +1 | tr -s " " | cut -d " " -f 7)
 sed -i "s/listen=.*/listen=udp:${HOST_IP}:5060/g" /usr/local/etc/opensips/opensips.cfg
+/usr/local/sbin/opensipsctl start
+
 tail -f /var/log/opensips.log
 
